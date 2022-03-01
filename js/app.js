34fc85d98386c2552768ca770b566223654f7394
Giovanny Canasto <giovannyc28@gmail.com>
2022-03-01 18:02:00 -0500

    ajuste sobre envio de email y registro de contrato

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log('local::::::' + localStorage.token);
-var urlBase = window.location.origin.replace('naser', 'apinaser').replace('8080', '8000') + "/api/";
+var urlBase = window.location.origin.replace('naser', 'apinaser').replace('8080', '8000') + "/public/api/";
 var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
