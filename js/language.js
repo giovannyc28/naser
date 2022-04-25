3a585931bd27fbcbd4af1ff1e1a9f2e527c772b1
Giovanny Canasto <giovannyc28@gmail.com>
2022-04-25 14:33:49 -0500

    prueba produccion

--- js/language.js
+++ js/language.js
@@ -63,14 +63,14 @@ function changeLanguage() {
         .catch(error => console.log('error', error));
     getMaritalStatus();
     getRelationShips();
-    getRelationShipsCte();
-    getPaises('#paisResidencia')
-        /*getPaises('#paisOrigen')
-        getPaises('#benPaisResidencia')
-        getPaises('#benPaisOrigen')
-        getPaises('#ctePais')
-        getPaises('#infoPais')
-        getPaises('#infoPais')*/
+    //getRelationShipsCte();
+    //getPaises('#paisResidencia')
+    /*getPaises('#paisOrigen')
+    getPaises('#benPaisResidencia')
+    getPaises('#benPaisOrigen')
+    getPaises('#ctePais')
+    getPaises('#infoPais')
+    getPaises('#infoPais')*/
 
 }
 changeLanguage();
