5e54483f6536d966e239f5f5c0987e864bc075ee
Giovanny Canasto <giovannyc28@gmail.com>
2022-08-31 16:49:08 -0500

    adicionado condicion de erver en app "

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 console.log('local::::::' + localStorage.token);
 var urlBase = window.location.origin.replace('portal.', 'api.').replace('8080', '8000') + "/public/api/";
+if (window.location.origin == "http://naser.local:8080")
+    var urlBase = "http://apinaser.local:8000/public/api/"
+if (window.location.origin == "https://www.naser.ibitsoluciones.com")
+    var urlBase = "https://www.apinaser.ibitsoluciones.com/public/api/"
 var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
